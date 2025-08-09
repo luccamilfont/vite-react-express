441ae3224d2f6888a903c57bbb7aa695df3ad72c
Lucca Araújo Milfont <luccamilfont@gmail.com>
2025-08-09 14:33:26 -0300

    fix: migration naming

--- express-api/db/migrations/20250809162145-create-OrderProducts.js
+++ express-api/db/migrations/20250809162145-create-OrderProducts.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('OrderProduct', {
+    await queryInterface.createTable('OrderProducts', {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
